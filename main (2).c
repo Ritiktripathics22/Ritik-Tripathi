fafea6a435d2a957d8016e7702cee3b8852d7aec
Ritiktripathics22 <125354056+Ritiktripathics22@users.noreply.github.com>
2023-02-15 11:10:21 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int l,b,area;
+   printf("enter length\n enter breadth\n");
+   scanf("%d%d",&l,&b);
+   area=l*b;
+   printf("area=%d",area);
+    return 0;
+}
